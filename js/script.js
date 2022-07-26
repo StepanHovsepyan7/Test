0f369c79b265456dc03259c581cf4b6f74101429
StepanHovsepyan7 <stpnhovsepyan@gmail.com>
2022-07-26 22:25:20 +0400

    First Try on Test Page

---
+++ js/script.js
@@ -0,0 +1,21 @@
+"use strict";
+
+$(() => {
+    let btnStart = $('.btnStart');
+    btnStart.click(function(){
+        $('.start').css({
+            display: 'none',
+        })
+        $('.parent').css({
+            display: 'block',
+        })
+    })
+
+    let winter = $("#winter");
+    let summer = $("#summer");
+    let autumn = $("#autumn");
+    let spring = $("#spring");
+
+    
+
+});
